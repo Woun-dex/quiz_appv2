c223b2aa125932b39d27f5d85973938ba1176747
walid d <walidelabad@gmail.com>
2024-11-16 22:04:55 +0100

    First commit

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='  flex justify-between items-center px-4 py-3 '>
+        <h1 className='font-bold'>Quiz App</h1>
+        <button className='btn'>
+            Login
+        </button>
+    </div>
+  )
+}
+
+export default Navbar
